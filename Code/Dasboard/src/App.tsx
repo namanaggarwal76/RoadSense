77b854fdf4a67f7826639c8c59a22c5cf3b22125
Naman Aggarwal <namanaggarwal76@gmail.com>
2025-09-09 16:40:00 +0530

    Develop dashboard main UI components

---
+++ Code/Dasboard/src/App.tsx
@@ -0,0 +1,35 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FleetProvider } from "@/contexts/FleetContext";
+import Index from "./pages/Index";
+import SafetyScoreDemo from "./pages/SafetyScoreDemo";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <FleetProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/safety" element={<SafetyScoreDemo />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </FleetProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
+);
+
+export default App;
